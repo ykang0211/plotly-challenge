4ce991893ba18341a465b5b8b143f4c6ee5c261d
ohyon <YKANG0211@GMAIL.COM>
2019-12-09 20:03:48 -0600

    init homework3

--- app.js
+++ app.js
@@ -1,62 +1,4 @@
-function buildPlot() {
-  d3.json("samples.json").then(function(data) {
-    console.log(data);
 
-    var bar_values = data.sample_values.slice(0,10);
-    var bar_labels = data.otu_ids.slice(0,10);
-    var bar_hover = data.otu_labels.slice(0,10);
-
-    var data = [{
-      x: bar_values,
-      y: bar_labels,
-      hovertext: bar_hover,
-      type: 'bar',
-      orientation: 'h'
-    }];
-
-    Plotly.newPlot("bar", data);
-
-  });
-};
-
-buildPlot();
-
-  
-// Build Bubble Chart
-function buildChart() {
-  d3.json("samples.json").then(function(data) {
-    console.log(data);
-    
-    var x = data.otu_ids;
-    var y = data.sample_values;
-    var markerSize = data.sample_values;
-    var markerColors = data.otu_ids; 
-    var textValues = data.otu_labels;
-
-    console.log(data.otu_ids);
-
-    var trace1 = {
-      x: x,
-      y: y,
-      text: textValues,
-      mode: 'markers',
-      marker: {
-        color: markerColors,
-        size: markerSize
-      } 
-    };
-  
-    var data = [trace1];
-
-    var layout = {
-      xaxis: { title: "OTU ID"},
-    };
-
-    Plotly.newPlot("bubble", data, layout);
-
-  });
-};
-buildChart();
 
 
 
@@ -74,29 +16,93 @@ function init() {
     });
 
     const firstSample = sampleNames[0];
-    buildCharts(firstSample);
+    buildPlot(firstSample);
     buildMetadata(firstSample);
   });
+
+  function buildMetadata() {
+    d3.json("samples.json").then(function(sample) {
+    
+  
+      var demographic = d3.select("#sample-metadata");
+  
+  
+      demographic.html("");
+  
+      Object.entries(sample).forEach(function([key, value]) {
+        var row = demographic.append("p");
+        row.text(`${key}: ${value}`);
+      });
+    });
+  };
+
+  function buildPlot() {
+    d3.json("samples.json").then(function(data) {
+      console.log(data);
+  
+      var bar_values = data.sample_values.slice(0,10);
+      var bar_labels = data.otu_ids.slice(0,10);
+      var bar_hover = data.otu_labels.slice(0,10);
+  
+      var data = [{
+        x: bar_values,
+        y: bar_labels,
+        hovertext: bar_hover,
+        type: 'bar',
+        orientation: 'h'
+      }];
+  
+      Plotly.newPlot("bar", data);
+  
+    });
+  };
+  
+  // buildPlot();
+  
+    
+  // Build Bubble Chart
+  function buildChart() {
+    d3.json("samples.json").then(function(data) {
+      console.log(data);
+      
+      var x = data.otu_ids;
+      var y = data.sample_values;
+      var markerSize = data.sample_values;
+      var markerColors = data.otu_ids; 
+      var textValues = data.otu_labels;
+  
+      console.log(data.otu_ids);
+  
+      var trace1 = {
+        x: x,
+        y: y,
+        text: textValues,
+        mode: 'markers',
+        marker: {
+          color: markerColors,
+          size: markerSize
+        } 
+      };
+    
+      var data = [trace1];
+  
+      var layout = {
+        xaxis: { title: "OTU ID"},
+      };
+  
+      Plotly.newPlot("bubble", data, layout);
+  
+    });
+  };
+  // buildChart();
+
 };
 
 function optionChanged(newSample) {
-  buildCharts(newSample);
+  buildPlot(newSample);
   buildMetadata(newSample);
 };
 
-
-function buildMetadata() {
-  d3.json("samples.json").then(function(sample) {
-  
-
-    var demographic = d3.select("#sample-metadata");
+init();
 
 
-    demographic.html("");
-
-    Object.entries(sample).forEach(function([key, value]) {
-      var row = demographic.append("p");
-      row.text(`${key}: ${value}`);
-    });
-  });
-};
